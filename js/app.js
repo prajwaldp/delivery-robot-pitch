57663a2d48ae05978a4b975b2dc3a4c7eea6d9c5
Prajwal Prakash <prajwalpy6@gmail.com>
2017-03-09 22:33:12 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+  $('.page-scroll a').bind('click', function(event) {
+
+    event.preventDefault();
+
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+    
+  });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+  target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+  $('.navbar-toggle:visible').click();
+});
